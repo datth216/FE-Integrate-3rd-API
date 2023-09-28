f24ee04484a8f35d13cf3063eedbf1a173f4dd73
datth216 <datwin0147@gmail.com>
2023-09-28 10:44:28 +0700

    update total component

---
+++ src/features/Order/components/Total.jsx
@@ -0,0 +1,36 @@
+import { EuroCircleOutlined } from '@ant-design/icons'
+import { Card, Col, Form, Input, Row } from 'antd'
+import React from 'react'
+
+function Total() {
+  return (
+    <Card title='Total' extra bordered={false}>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32
+        }}
+      >
+        <Col className='gutter-row' span={24}>
+          <Form.Item
+            label='Total Amount'
+            name='totalAmount'
+            rules={[
+              {
+                min: 0,
+                required: true,
+                message: 'Total amout is required!'
+              }
+            ]}
+          >
+            <Input prefix={<EuroCircleOutlined />} disabled className='text-lg font-bold' />
+          </Form.Item>
+        </Col>
+      </Row>
+    </Card>
+  )
+}
+
+export default Total
